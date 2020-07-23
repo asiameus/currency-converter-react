84081e48994cadf6ebddbf2c991dc043480d1e25
Asia Meus <joanna@meus.pl>
2020-07-23 13:15:03 +0200

    Updates

---
+++ precache-manifest.73886e46f2b7a2f84cd4e5e3cbcd8efa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08fba838ab432d522bfb73b762dbfd76",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "78489d06be61cf9d04a2",
+    "url": "/currency-converter-react/static/css/main.46295cd8.chunk.css"
+  },
+  {
+    "revision": "df18eda2b5fc548341b9",
+    "url": "/currency-converter-react/static/js/2.abfbd1d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.abfbd1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78489d06be61cf9d04a2",
+    "url": "/currency-converter-react/static/js/main.8f751938.chunk.js"
+  },
+  {
+    "revision": "07b2973be230e9c120d7",
+    "url": "/currency-converter-react/static/js/runtime-main.2415008a.js"
+  },
+  {
+    "revision": "c61f9702af2ff1e2ecddea7e2f3ceb2b",
+    "url": "/currency-converter-react/static/media/background_1.c61f9702.jpg"
+  }
+]);
